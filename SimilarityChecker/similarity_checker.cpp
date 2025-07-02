38b08bfbf1ad374de5e2a03afe27dc202d2fb153
soonwonchoi <soonwon.choi@samsung.com>
2025-07-02 15:29:43 +0900

    [feature] Case: Same Length

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -1,5 +1,12 @@
+#include <string>
+
+using std::string;
+
 class SimilarityChecker
 {
 public:
-
+	int checkSimilarity(string a, string b)
+	{
+		return 60;
+	}
 };
--- SimilarityChecker/similarity_checker_test.cpp
+++ SimilarityChecker/similarity_checker_test.cpp
@@ -1,6 +1,12 @@
 #include "gmock/gmock.h"
 #include "similarity_checker.cpp"
 
+TEST(SimilarityChecker, SameLength)
+{
+	SimilarityChecker simchecker;
+	EXPECT_EQ(60, simchecker.checkSimilarity("ABCDE", "ABCDE"));
+}
+
 int main()
 {
 	testing::InitGoogleMock();
